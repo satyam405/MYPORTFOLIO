c2f6c3d6d7ffc714c4ec81e6dc8509f3e52104eb
Shivam Kumar <shivam.kumar@gmail.com>
2020-05-24 19:17:30 -0600

    initial project done

---
+++ js/custom.js
@@ -0,0 +1,119 @@
+/*
+  * @package 
+  * @subpackage template name HTML
+  * 
+  * Template Scripts
+  * Created by themeturn
+  
+  1. Fixed header
+  2. Site search
+  3. Main slideshow
+  4. Owl Carousel
+      a. Testimonial
+      b. Clients
+      c. Team
+  5. Back to top
+  6. Skills
+  7. BX slider
+      a. Blog Slider
+      b. Portfolio item slider
+  8. Isotope
+  9. Animation (wow)
+  10. Flickr
+  
+*/
+
+
+jQuery(function($) {
+  "use strict";
+
+   $('.navigation').singlePageNav({
+        currentClass: 'active',
+        changeHash: true,
+        scrollSpeed: 750,
+        offset: 0,
+        filter: ':not(.external)',
+        easing: 'swing',
+
+    });
+
+    $.noConflict();
+     $('.nav a').on('click', function(){ 
+        if($('.navbar-toggle').css('display') !='none'){
+            $(".navbar-toggle").trigger( "click" );
+        }
+    });
+
+
+ 
+// prettyphoto
+
+ $("a[data-rel^='prettyPhoto']").prettyPhoto();
+ 
+    
+ 
+
+
+
+});
+
+
+//made by vipul mirajkar thevipulm.appspot.com
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
+
